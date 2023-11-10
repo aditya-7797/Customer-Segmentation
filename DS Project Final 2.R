593d7c83a66827ea362e4a7751de4787de22f6f2
aditya-7797 <148460051+aditya-7797@users.noreply.github.com>
2023-11-10 19:37:32 +0530

    Add files via upload

---
+++ DS Project Final 2.R
@@ -0,0 +1,105 @@
+library(shiny)
+library(dplyr)
+library(DT)
+library(plotly)
+library(stats)
+
+# Load the customer data
+customer_data <- read.csv("C:/DS NOTES/Mall_Customers.csv")
+
+# Define the UI
+ui <- fluidPage(
+  titlePanel("Customer Data Analysis and Clustering"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("analysis", "Choose Analysis",
+                  choices = c("Gender Visualization", "Age Distribution", "Annual Income Distribution", "K-means Clustering")),
+      conditionalPanel(
+        condition = "input.analysis == 'K-means Clustering'",
+        numericInput("k_clusters", "Number of Clusters (K):", value = 3)
+      ),
+      plotOutput("visualization")
+    ),
+    mainPanel(
+      DTOutput("cluster_table"),
+      plotlyOutput("kmeans_clusters")  # Output for K-means clustering
+    )
+  )
+)
+
+# Define the server
+server <- function(input, output) {
+  
+  # K-means clustering
+  kmeans_clusters <- reactive({
+    if (input$analysis == "K-means Clustering") {
+      # Perform K-means clustering with the specified number of clusters (K)
+      kmeans_model <- kmeans(customer_data[, c("Age", "Annual.Income..k..")], centers = input$k_clusters)
+      customer_data$Cluster <- as.factor(kmeans_model$cluster)
+      
+      # Create a scatter plot with color-coded clusters
+      kmeans_plot <- plot_ly(customer_data, x = ~Age, y = ~Annual.Income..k.., color = ~Cluster) %>%
+        add_markers()
+      
+      # Return the plot and the clustering results
+      list(kmeans_plot, customer_data)
+    } else {
+      # Return NULL if K-means clustering is not selected
+      NULL
+    }
+  })
+  
+  # Output for K-means clustering
+  output$kmeans_clusters <- renderPlotly({
+    if (!is.null(kmeans_clusters())) {
+      kmeans_plot <- kmeans_clusters()[[1]]
+      kmeans_plot
+    }
+  })
+  
+  # Other output components
+  output$cluster_table <- renderDT({
+    if (input$analysis == "Gender Visualization") {
+      # Create a summary table with counts of male and female
+      gender_counts <- table(customer_data$Gender)
+      data.frame(Gender = c("Female", "Male"), Count = gender_counts)
+    } else if (input$analysis == "Age Distribution") {
+      # Display the Age column for age distribution
+      data.frame(Age = customer_data$Age)
+    } else if (input$analysis == "Annual Income Distribution") {
+      # Display the Annual Income column for income distribution
+      data.frame(Annual.Income = customer_data$Annual.Income..k..)
+    }
+  })
+  
+  output$visualization <- renderPlotly({
+    if (input$analysis == "Gender Visualization") {
+      a <- table(customer_data$Gender)
+      barplot(a, main = "Gender Comparison",
+              ylab = "Count",
+              xlab = "Gender",
+              col = rainbow(2),
+              ylim = c(0, 1200),  # Set the y-axis limit to 120
+              legend = rownames(a))
+      # Add text labels for total count above the bars
+      text(1:2, a, labels = a, pos = 3, cex = 1.2)
+    } else if (input$analysis == "Age Distribution") {
+      hist(customer_data$Age,
+           col = "blue",
+           main = "Age Distribution",
+           xlab = "Age",
+           ylab = "Frequency",
+           labels = TRUE)
+    } else if (input$analysis == "Annual Income Distribution") {
+      hist(customer_data$Annual.Income..k..,
+           col = "#660033",
+           main = "Annual Income Distribution",
+           xlab = "Annual Income",
+           ylab = "Frequency",
+           labels = TRUE)
+    }
+  })
+}
+
+# Run the Shiny app
+shinyApp(ui, server)
